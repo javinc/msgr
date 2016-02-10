684318f0769bd58fdfcc117a23cf1a62ad810196
javinc <javinczki02@gmail.com>
2016-02-11 03:36:52 +0800

    app bar and nav

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,19 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 import NavLink from './NavLink';
 
+var NavMenu = React.createClass({
+    render() {
+        return (
+            <NavLink {...this.props}>
+                <MenuItem>
+                    {this.props.children}
+                </MenuItem>
+            </NavLink>
+        )
+    }
+});
+
 export default React.createClass({
+    getInitialState() {
+        return {
+            navOpen: false
+        };
+    },
+
+    toogleLeftNav() {
+        // toggle
+        this.setState({
+            navOpen: !this.state.navOpen
+        });
+    },
+
+    closeLeftNav() {
+        // toggle
+        this.setState({
+            navOpen: false
+        });
+    },
+
     render() {
         return (
             <div>
-                <h1>Main Page</h1>
-                <ul>
-                    <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/repos">Repos</NavLink></li>
-                    <li><NavLink to="/login">Logout</NavLink></li>
-                </ul>
+                <AppBar
+                    title="Msgr"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={this.toogleLeftNav}
+                />
+
+                <LeftNav
+                    docked={false}
+                    open={this.state.navOpen}>
+                    <NavMenu onTouchTap={this.closeLeftNav} to="/" onlyActiveOnIndex>Home</NavMenu>
+                    <NavMenu onTouchTap={this.closeLeftNav} to="/about">About</NavMenu>
+                    <NavMenu onTouchTap={this.closeLeftNav} to="/repos">Repos</NavMenu>
+                    <NavMenu onTouchTap={this.closeLeftNav} to="/login">Logout</NavMenu>
+                </LeftNav>
 
                 {/* content */}
                 {this.props.children}
